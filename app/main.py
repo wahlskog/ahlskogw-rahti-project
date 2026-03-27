dd512c877db2717a21e0e12a63799f7216fd288d
wahlskog <ahlskogw@arcada.fi>
2026-03-27 12:08:57 +0200

    Added ip

--- app/main.py
+++ app/main.py
@@ -1,12 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return { "msg": "Morjens Ahlskogw!", "v": "0.2" }
+    return { "msg": "Hello local docker"}
+
+@app.get("/api/ip")
+def ip(request: Request):
+    return { "ip": request.client.host}
 
 
-@app.get("/items/{id}")
-def read_item(item_id: int, q: str = None):
-    return {"id": id, "q": q}
+@app.get("/ip", response_class=HTMLResponse)
+def ip(request: Request):
+    return f"<h1>Din ip är {request.client.host}</h1>"
+
+@app.get("/hello")
+def hello():
+    return { "msg": "Hello fredde"}
